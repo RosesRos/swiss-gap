4373196f2aa22c1690a769fd17d51ca47e6972d8
facesar <rostranespinoza@gmail.com>
2023-01-18 23:14:48 +0300

    update

--- home.php
+++ home.php
@@ -2,6 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php header("Cache-Control: no-cache, max-age=0, must-revalidate, no-store");?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
