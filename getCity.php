aede756462f816ec89946125333fb185b00cca5c
Rsesros <rostranespinoza@gmail.com>
2023-03-21 13:54:56 +0300

    update

--- getCity.php
+++ getCity.php
@@ -23,3 +23,42 @@ $city = $data->city;
 print_r('your city' . ' ' . $city);
 
 ?>
+
+
+
+<?php
+                $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+                // $ip_address = '196.196.53.84';
+
+            ?>
+            <script>
+                let ip = '<?= $ip_address ;?>'
+
+                let p = document.getElementById('loca');
+
+                var IPs = [`${ip}`];
+
+                // ip-api endpoint URL
+                // see http://ip-api.com/docs/api:batch for documentation
+                var endpoint = 'https://api.techniknews.net/ipgeo/';
+
+                var xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        // Result array
+                        var response = JSON.parse(this.responseText);
+                        // console.log(response);
+                        p.innerHTML = response.city
+                        // response.forEach(function (item) {
+                        //     console.log(item.query + " is in " + item.city + ", " + item.country); 
+                        //     p.innerHTML = item.city
+                        // });
+                    }
+                };
+                var data = JSON.stringify(IPs);
+                // console.log("sending:", data);
+
+                xhr.open('POST', endpoint, true);
+                xhr.send(data);
+
+            </script>
